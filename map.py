5dc8fc39edbb58fe1358fed25ffb82927b6b5d1d
Siwat_Keng <siwat.keng.pongpanit@gmail.com>
2020-03-29 17:29:16 +0700

    collect map

--- map.py
+++ map.py
@@ -1,13 +1,13 @@
 from pyrplidar import RPLidar
 import numpy as np
-import cv2, time
+import cv2, time, winsound
 
 time.sleep(10)
 rplidar = RPLidar()
 mapping = np.zeros((16000,16000,3),np.uint8)
 iterator = rplidar.iter_scan_points()
 
-for i in range(1000):
+for i in range(8000):
     scan = next(iterator)
     for i in scan:
         mapping[int(i[1]+8000),int(i[0]+8000)] += 1
@@ -18,7 +18,11 @@ rplidar.disconnect()
  
 mapping = mapping[4000:12000,4000:12000]
 mapping = cv2.cvtColor(mapping, cv2.COLOR_BGR2GRAY)
-ret, mapping = cv2.threshold(mapping, 10, 255, cv2.THRESH_BINARY)
+ret, mapping = cv2.threshold(mapping, 3, 255, cv2.THRESH_BINARY)
 contours, hierarchy = cv2.findContours(mapping, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 cv2.drawContours(mapping, contours, -1, (255,255,255), 5)
 cv2.imwrite('map.png', mapping)
+
+while True:
+    winsound.Beep(1500, 500)
+    cv2.waitKey(1)
